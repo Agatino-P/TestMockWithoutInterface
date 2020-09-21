ef5d94c93e6df98cc823c7a1f1137b4160b4f157
agatino.work@gmail.com <agatino.work@gmail.com>
2020-09-21 13:05:28 +0200

    Add project files.

---
+++ NotifierProj/Notifier.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NotifierProj
+{
+    public class Notifier
+    {
+        private Action<IEnumerable<string>> _notifyAction;
+
+        public Notifier( Action<IEnumerable<string>> notifyAction)
+        {
+            _notifyAction = notifyAction;
+        }
+
+        public void DoNotify(IEnumerable<string> news)
+        {
+            _notifyAction?.Invoke(news);
+        }
+    }
+}
---
+++ TestMockApp/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using NotifierProj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace TestMockApp
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private List<string> _strings = new List<string>() { "a", "b" };
+        private Notifier _notifier;
+        
+        public MainWindowViewModel()
+        {
+            _notifier = new Notifier(OnNotify);
+        }
+
+        private RelayCommand _callNotifierCmd;
+        public RelayCommand CallNotifierCmd => _callNotifierCmd ?? (_callNotifierCmd = new RelayCommand(
+            () => callNotifier(),
+            () => { return 1 == 1; },
+            keepTargetAlive: true
+            ));
+        private void callNotifier()
+        {
+            _notifier.DoNotify(_strings);
+        }
+
+        public void OnNotify(IEnumerable<string> news)
+            {
+            string message= string.Join(", ", news);
+            MessageBox.Show(message);
+            }
+
+
+    }
+}
